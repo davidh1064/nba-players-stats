49d17c7282675021c522ff70a946d648226248b9
davidh1064 <huangdavid10@gmail.com>
2025-03-17 13:49:22 -0700

    added PlayerService methods

--- backend/src/main/java/com/nba/nba_zone/player/PlayerService.java
+++ backend/src/main/java/com/nba/nba_zone/player/PlayerService.java
@@ -19,9 +19,55 @@ public class PlayerService {
         return playerRepository.findAll();
     }
 
+    private Player getPlayerById(Long id) {
+        return playerRepository.findById(id).orElse(null);
+    }
+
     public List<Player> getPlayersFromTeam(String teamName) {
         return playerRepository.findAll().stream()
                 .filter(player -> teamName.equals(player.getTeamAbbreviation()))
                 .collect(Collectors.toList());
+    }
+
+    public List<Player> getPlayersByName(String searchText) {
+        return playerRepository.findAll().stream()
+                .filter(player -> player.getPlayerName().toLowerCase().contains(searchText.toLowerCase()))
+                .collect(Collectors.toList());
+    }
+
+    private Player addPlayer(Player player) {
+        return playerRepository.save(player);
+    }
+
+    private Player updatePlayer(Long id, Player playerDetails) {
+        Player existingPlayer = playerRepository.findPlayerById(id)
+                .orElseThrow(() -> new RuntimeException("Player not found"));
+        existingPlayer.setPlayerName(playerDetails.getPlayerName());
+        existingPlayer.setTeamAbbreviation(playerDetails.getTeamAbbreviation());
+        existingPlayer.setAge(playerDetails.getAge());
+        existingPlayer.setPlayerHeight(playerDetails.getPlayerHeight());
+        existingPlayer.setPlayerWeight(playerDetails.getPlayerWeight());
+        existingPlayer.setCollege(playerDetails.getCollege());
+        existingPlayer.setCountry(playerDetails.getCountry());
+        existingPlayer.setDraftYear(playerDetails.getDraftYear());
+        existingPlayer.setDraftRound(playerDetails.getDraftRound());
+        existingPlayer.setDraftNumber(playerDetails.getDraftNumber());
+        existingPlayer.setGp(playerDetails.getGp());
+        existingPlayer.setPts(playerDetails.getPts());
+        existingPlayer.setReb(playerDetails.getReb());
+        existingPlayer.setAst(playerDetails.getAst());
+        existingPlayer.setNetRating(playerDetails.getNetRating());
+        existingPlayer.setOrebPct(playerDetails.getOrebPct());
+        existingPlayer.setDrebPct(playerDetails.getDrebPct());
+        existingPlayer.setUsgPct(playerDetails.getUsgPct());
+        existingPlayer.setTsPct(playerDetails.getTsPct());
+        existingPlayer.setAstPct(playerDetails.getAstPct());
+        existingPlayer.setSeason(playerDetails.getSeason());
+
+        return playerRepository.save(existingPlayer);
+    }
+
+    private void deletePlayer(Long id) {
+        playerRepository.deletePlayerById(id);
     }
 }
