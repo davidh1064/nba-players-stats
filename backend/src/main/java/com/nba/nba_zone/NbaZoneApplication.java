0e41f2dc53126a6d9f3e76b1d9ffb8ae773f3d03
davidh1064 <huangdavid10@gmail.com>
2025-03-17 15:11:56 -0700

    updated gitignore file and added dotenv dependency

--- backend/src/main/java/com/nba/nba_zone/NbaZoneApplication.java
+++ backend/src/main/java/com/nba/nba_zone/NbaZoneApplication.java
@@ -2,11 +2,21 @@ package com.nba.nba_zone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class NbaZoneApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure()
+				.filename(".env")
+				.load();
+
+		// Set loaded variables to System Properties
+		System.setProperty("DB_URL", dotenv.get("DB_URL"));
+		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+
 		SpringApplication.run(NbaZoneApplication.class, args);
 	}
 
